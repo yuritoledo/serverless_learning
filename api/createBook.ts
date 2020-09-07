15930198a3646ca4da39e6be35f5556d4a8c4d4a
Yuri Toledo <yuriwtoledo@gmail.com>
2020-09-07 18:18:19 -0300

    chore: fixing database env

--- api/createBook.ts
+++ api/createBook.ts
@@ -1,13 +1,12 @@
 import { NowRequest, NowResponse } from '@vercel/node'
 import { PrismaClient } from '@prisma/client'
+
 const prisma = new PrismaClient()
 
 export default async (req: NowRequest, res: NowResponse) => {
   const name = String(req.query.name || 'yuri')
   const book = await prisma.books.create({
-    data: {
-      name
-    }
+    data: { name }
   })
-  res.status(200).json({ book })
+  res.json({ book })
 }
