ad4ce28ee5733d19453d911fc09842bacf097b13
Yuri Toledo <yuriwtoledo@gmail.com>
2020-09-07 16:36:19 -0300

    starting

---
+++ src/getBooks.ts
@@ -0,0 +1,12 @@
+async function getBooks() {
+  const res = [
+    { book: 'asuhduahsd' },
+    { book: 'asuhduahsd2' },
+    { book: 'asuhduahsd3' },
+    { book: 'asuhduahsd4' },
+  ]
+
+  return res
+}
+
+export default getBooks
