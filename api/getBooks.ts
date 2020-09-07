65e98ad48737be70dc96672cfc0760cae99520dd
Yuri Toledo <yuriwtoledo@gmail.com>
2020-09-07 18:04:57 -0300

    testing

--- api/getBooks.ts
+++ api/getBooks.ts
@@ -1,5 +1,5 @@
 import { NowRequest, NowResponse } from '@vercel/node'
 
-export default (req: NowRequest, res: NowResponse) => {
-  return res.json({ name: 'John', email: 'john@example.com' })
+export default async (req: NowRequest, res: NowResponse) => {
+  res.json({ name: 'John', email: 'john@example.com' })
 }
