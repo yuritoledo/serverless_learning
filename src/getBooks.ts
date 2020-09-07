9ac4e3a40f5100dd6206e7abfb93377749a6e0f1
Yuri Toledo <yuriwtoledo@gmail.com>
2020-09-07 16:45:42 -0300

    tests

--- src/getBooks.ts
+++ src/getBooks.ts
@@ -1,15 +1,21 @@
+// import { NowRequest, NowResponse } from '@vercel/node'
+
+// async function getBooks(req: NowRequest, res: NowResponse) {
+
+//   const list = [
+//     { book: 'asuhduahsd' },
+//     { book: 'asuhduahsd2' },
+//     { book: 'asuhduahsd3' },
+//     { book: 'asuhduahsd4' },
+//   ]
+
+//   res.status(200).json(list)
+// }
+
+// export default getBooks
+
 import { NowRequest, NowResponse } from '@vercel/node'
 
-async function getBooks(req: NowRequest, res: NowResponse) {
-
-  const list = [
-    { book: 'asuhduahsd' },
-    { book: 'asuhduahsd2' },
-    { book: 'asuhduahsd3' },
-    { book: 'asuhduahsd4' },
-  ]
-
-  res.status(200).json(list)
+export default (req: NowRequest, res: NowResponse) => {
+  res.json({ name: 'John', email: 'john@example.com' })
 }
-
-export default getBooks
