c912c4fa5ba0a72cef4df189dcc2fb12dddfa343
Yuri Toledo <yuriwtoledo@gmail.com>
2020-09-07 17:00:25 -0300

    chore: add tsconfig, now it goes :thinking:

--- src/getBooks.ts
+++ src/getBooks.ts
@@ -1,19 +1,3 @@
-// import { NowRequest, NowResponse } from '@vercel/node'
-
-// async function getBooks(req: NowRequest, res: NowResponse) {
-
-//   const list = [
-//     { book: 'asuhduahsd' },
-//     { book: 'asuhduahsd2' },
-//     { book: 'asuhduahsd3' },
-//     { book: 'asuhduahsd4' },
-//   ]
-
-//   res.status(200).json(list)
-// }
-
-// export default getBooks
-
 import { NowRequest, NowResponse } from '@vercel/node'
 
 export default (req: NowRequest, res: NowResponse) => {
