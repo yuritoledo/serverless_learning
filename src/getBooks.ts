484a90f8200b4e19d41b0d8c1927e6e77eff0a5c
Yuri Toledo <yuriwtoledo@gmail.com>
2020-09-07 16:40:59 -0300

    refac: update getbooks function

--- src/getBooks.ts
+++ src/getBooks.ts
@@ -1,12 +1,14 @@
-async function getBooks() {
-  const res = [
+import { NowRequest, NowResponse } from '@vercel/node'
+
+async function getBooks(req: NowRequest, res: NowResponse) {
+  const list = [
     { book: 'asuhduahsd' },
     { book: 'asuhduahsd2' },
     { book: 'asuhduahsd3' },
     { book: 'asuhduahsd4' },
   ]
 
-  return res
+  res.status(200).json(list)
 }
 
 export default getBooks
